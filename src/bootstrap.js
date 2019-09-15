dcd7d1170851cd18d6074870d7445cb386493411
hussnain <hussnainwc@gmail.com>
2019-09-16 09:19:35 +1000

    updated dependencies

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,50 +1,39 @@
 import Vue from 'vue'
-import axios from 'axios'
-import VueRouter from 'vue-router'
-import { Form, HasError, AlertError } from 'vform'
-import VueSweetalert2 from 'vue-sweetalert2';
-import Swal from 'sweetalert2'
-import VueProgressBar from 'vue-progressbar'
-import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
-
-import User from './helpers/User'
-import Storage from './helpers/Storage'
-
 window.Vue = Vue
 Vue.config.productionTip = false
 
-window.Vuex = Vuex
-Vue.use(Vuex)
+import axios from 'axios'
+window.axios = axios
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+Vue.use(Vuex)
+window.Vuex = Vuex
 window.createPersistedState = createPersistedState
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import Swal from 'sweetalert2'
 Vue.use(VueSweetalert2);
+window.Swal = Swal
+window.Toast = Toast
 const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
   showConfirmButton: false,
   timer: 3000
-});
+  });
 
+import VueProgressBar from 'vue-progressbar'
 Vue.use(VueProgressBar, {
   color: '#bffaf3',
   failedColor: 'red',
   height: '3px'
 })
 
-window.Form = Form
-Vue.component(HasError.name, HasError)
-Vue.component(AlertError.name, AlertError)
-
-window.Vue = Vue
-
-Vue.use(VueRouter)
-
-window.axios = axios
-
-window.Swal = Swal
-window.Toast = Toast
-
+import User from './helpers/User'
+import Storage from './helpers/Storage'
 window.Storage = Storage
 window.User = User
