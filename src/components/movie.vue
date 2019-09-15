30ba064d25112cb46f7e89888e07befad52622c9
hussnain <hussnainwc@gmail.com>
2019-09-16 02:10:40 +1000

    computed properties

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -9,7 +9,7 @@ Vue.component("movie",{
       <br>
       <br>
       <br>
-      <span v-size="'30px'" class="tittle"> {{movie.tittle | catch("Title") | capitalize }} </span>
+      <span v-size="'30px'" class="tittle"> {{ movie.tittle | catch("Title") | capitalize }} </span>
       <br>
       <br>
       <br>
@@ -21,25 +21,25 @@ Vue.component("movie",{
       <br>
       <br>
       <br>
-      <span v-size="'20px'" v-color="'#ffffff'"> YEAR : {{movie.year | catch("Year")}}</span>
+      <span v-size="'20px'" v-color="'#ffffff'"> YEAR : {{ year | catch("Year")}}</span>
       <br>
       <br>
-      <span v-size="'20px'" v-color="'#ffffff'"> ROTTEN TOMATOES : {{movie.rating | catch("Rating")}}</span>
+      <span v-size="'20px'" v-color="'#ffffff'"> ROTTEN TOMATOES : {{ rating | percent |catch("Rating")}}</span>
       <br>
       <br>
-      <span v-size="'20px'" v-color="'#ffffff'"> PLOT : {{movie.plot | catch("Plot")}}</span>
+      <span v-size="'20px'" v-color="'#ffffff'"> PLOT : {{ movie.plot | catch("Plot")}}</span>
       <br>
       <br>
       <br>
       <br>
       <div v-if="path == '/now'" v-center>
-        <button class="button" type="button" name="button" value="Book Ticket">BOOK TICKET</button>
+        <button @click="buy()" class="button" type="button" name="button" value="Book Ticket">BOOK TICKET</button>
       </div>
       <div v-else-if="path == '/soon'" v-center>
-        <button class="button" type="button" name="button" value="">BOOK AHEAD</button>
+        <button @click="buy()" class="button" type="button" name="button" value="">BOOK AHEAD</button>
       </div>
       <div v-else v-center>
-        <button :class="{ 'red': path == '/' }" class="button" type="button" name="button" value="">SOLD</button>
+        <button @click="sold()" :class="{ 'red': path == '/' }" class="button" type="button" name="button" value="">SOLD</button>
       </div>
       <br>
       <br>
@@ -62,9 +62,82 @@ export default {
     this.movie = this.$store.getters.get;
     this.path = this.$store.getters.getPath;
   },
+  computed:{
+    year(){
+      if(this.path == '/now'){
+        return new Date().getFullYear();
+      }
+      else if(this.path == '/soon'){
+        return (new Date().getFullYear() + Math.floor(Math.random() * (+2 - +0)) + +0);
+      }
+      return this.movie.year
+    },
+    rating(){
+      if(this.movie.rating == "0%"){
+        return Math.floor(Math.random() * (+99 - +0)) + +0;
+      }
+      return this.movie.rating
+    }
+  },
   methods:{
     getImage(){
       return this.movie.poster;
+    },
+    buy(){
+      Swal.fire({
+        title: 'WOULD YOU LIKE TO BUY A TICKET?',
+        text: 'EACH TICKET COST $10',
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'BUY',
+        cancelButtonText: 'NO'
+      })
+        .then((result) => {
+              if (result.value) {
+                this.enterTickets();
+              }})
+    },
+    enterTickets(){
+      Swal.fire({
+        title: 'HOW MANY TICKETS WOULD YOU LIKE TO BUY!',
+        input: 'text',
+        confirmButtonText: 'Lovely!',
+        inputValidator: (value) => {
+          if (!value || value == 0) {
+            return 'ENTER A VALID NUMBER!'
+          }
+        }
+      }).then((result) =>{
+        if(result.value){
+          if(this.$store.getters.getCard != undefined){
+            this.chargeCard(result.value);
+          }
+          else{
+            Toast.fire({type: 'info',title: 'Please add a valid card '})
+          }
+        }
+      })
+    },
+    chargeCard(amount){
+      Swal.fire({
+        title: amount * 10 + ' DOLLARS WILL BE CHARGED' ,
+        text: " to your card : "+User.maskCard(this.$store.getters.getCard.number),
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'BUY',
+        cancelButtonText: 'NO'
+      })
+        .then((result) => {
+              if (result.value) {
+
+              }})
+    },
+    sold(){
+      Toast.fire({type: 'info',title: 'SOLD OUT'})
     }
   }
 }
